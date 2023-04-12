6a399bb09a6b2e17d844241fe1235fc622d2241e
SHAKOTN <shakotn@protonmail.com>
2023-04-12 21:30:30 +0300

    [chore] - upd comment

--- contracts/strategy/ConvexStrategy.sol
+++ contracts/strategy/ConvexStrategy.sol
@@ -830,7 +830,7 @@ contract ConvexStrategy {
         );
         // Check if slippage protection is enabled
         if (_slippage) {
-            // Calculate the ratio based on the curve
+            // Calculate the ratio based on the curve virtual price
             uint256 ratio = curveValue();
 
             // This represents the scaled meta_amount adjusted by the calculated ratio.
