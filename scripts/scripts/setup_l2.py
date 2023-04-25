98d77ad7826daa1a41fa80e369a4d61eeda7138a
SHAKOTN <shakotn@protonmail.com>
2023-04-25 15:11:41 +0300

    [feat] - adding rest of the contracts except for ConvexStrategy

--- scripts/scripts/setup_l2.py
+++ scripts/scripts/setup_l2.py
@@ -1,6 +1,7 @@
 import os
 from enum import Enum
 
+from brownie import ConvexStrategy  # noqa
 from brownie import CurveOracle  # noqa
 from brownie import GMigration  # noqa
 from brownie import GRouter  # noqa
@@ -11,14 +12,19 @@ from brownie import PnL  # noqa
 from brownie import PnLFixedRate  # noqa
 from brownie import RouterOracle  # noqa
 from brownie import SeniorTranche  # noqa
+from brownie import StopLossLogic  # noqa
 from brownie import accounts
 from brownie import web3  # noqa
+from brownie.network.account import Account
 from dotenv import load_dotenv
 from rich.console import Console
 
-from scripts.scripts.addresses import CHAINLINK_AGG_ADDRESSES
-from scripts.scripts.addresses import THREE_POOL_ADDRESS
-from scripts.scripts.addresses import THREE_POOL_TOKEN_ADDRESS
+# from rich.progress import track
+from scripts.scripts.addresses import POLYGON_CHAINLINK_AGG_ADDRESSES
+from scripts.scripts.addresses import POLYGON_THREE_POOL_ADDRESS
+from scripts.scripts.addresses import POLYGON_THREE_POOL_TOKEN_ADDRESS
+
+# from scripts.scripts.addresses import CONVEX_POOLS_POLYGON
 
 console = Console()
 load_dotenv()
@@ -56,9 +62,28 @@ def load_pkey(account_name: str = DEPLOYER) -> str:
         return accounts.at(os.getenv(account_name), force=True)
 
 
+def _deploy_frax_strategy(
+        deployer: Account,
+        vault: GVault,
+        snl: StopLossLogic,
+        convex_pool_id: int,
+        convex_lp_token: str,
+) -> ConvexStrategy:
+    """
+    Deploy convex strategy with given params and connect it to vault and stop loss logic
+    """
+    strategy = deployer.deploy(
+        ConvexStrategy, vault, deployer.address, convex_pool_id, convex_lp_token,
+        publish_source=False, silent=SILENT_DEPLOYMENT
+    )
+    console.log(f"ConvexStrategy deployed at {strategy.address}", style="bold cyan")
+    strategy.setKeeper(deployer, {"from": deployer})
+    strategy.setStopLossLogic(snl, {"from": deployer})
+
+
 def deploy_to_l2() -> None:
     """
-    Main script to deploy GRo protocol to L2
+    Main script to deploy GRo protocol to L2(Currently - Polygon)
     """
     console.log("Deploying G2 protocol...", style="bold blue")
     deployer = accounts.at(load_pkey(account_name=DEPLOYER))
@@ -67,7 +92,7 @@ def deploy_to_l2() -> None:
     # Deploy GVault
     console.log("Deploying GVault...", style="bold yellow")
     gro_vault = deployer.deploy(
-        GVault, THREE_POOL_TOKEN_ADDRESS, publish_source=False, silent=SILENT_DEPLOYMENT
+        GVault, POLYGON_THREE_POOL_TOKEN_ADDRESS, publish_source=False, silent=SILENT_DEPLOYMENT
     )
     console.log(f"GVault deployed at {gro_vault}", style="bold cyan")
 
@@ -108,7 +133,7 @@ def deploy_to_l2() -> None:
 
     console.log("Deploying RouterOracle", style="bold yellow")
     router_oracle = deployer.deploy(
-        RouterOracle, CHAINLINK_AGG_ADDRESSES, publish_source=False,
+        RouterOracle, POLYGON_CHAINLINK_AGG_ADDRESSES, publish_source=False,
         silent=SILENT_DEPLOYMENT
     )
     console.log(f"RouterOracle deployed at {router_oracle}", style="bold cyan")
@@ -118,8 +143,8 @@ def deploy_to_l2() -> None:
         gtranche,
         gro_vault,
         router_oracle,
-        THREE_POOL_ADDRESS,
-        THREE_POOL_TOKEN_ADDRESS,
+        POLYGON_THREE_POOL_ADDRESS,
+        POLYGON_THREE_POOL_TOKEN_ADDRESS,
         publish_source=False,
         silent=SILENT_DEPLOYMENT
     )
@@ -133,3 +158,17 @@ def deploy_to_l2() -> None:
     console.log(f"Setting PNL for Gtranche", style="bold yellow")
     gtranche.setPnL(pnl.address, {"from": deployer.address})
     console.log(f"PNL For Gtranche set at {pnl.address}", style="bold cyan")
+
+    console.log(f"Depoloying SnL", style="bold yellow")
+    snl = deployer.deploy(StopLossLogic, publish_source=False, silent=SILENT_DEPLOYMENT)
+    console.log(f"SnL deployed at {pnl}", style="bold cyan", log_locals=True)
+
+    # console.log(f"Deploying ConvexStrategies!", style="bold yellow")
+    # for pool_name, data in track(
+    #         CONVEX_POOLS_POLYGON.items(), description="Deploying CVX strats..."
+    # ):
+    #     console.log(
+    #         f"Deploying {pool_name} strategy, pid: {data['pid']}, token: {data['LP_TOKEN']}",
+    #         style="bold yellow"
+    #     )
+    #     _deploy_frax_strategy(deployer, gro_vault, snl, data['pid'], data['LP_TOKEN'])
