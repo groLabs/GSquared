2e45195f0c184173ddc04c975a2744ae4f0b3e0d
SHAKOTN <shakotn@protonmail.com>
2023-05-19 12:56:59 +0300

    [tests] - adding assertions to check stables balance

--- test/unittests/GRouterUnit.t.sol
+++ test/unittests/GRouterUnit.t.sol
@@ -20,10 +20,13 @@ contract RouterUnitTest is BaseUnitFixture {
         assertEq(gTranche.trancheBalances(0), 0);
         // Deal some DAI to alice
         dai.faucet(type(uint256).max / 2);
+        uint256 aliceInitBalance = dai.balanceOf(alice);
         dai.approve(address(gRouter), type(uint256).max);
         gRouter.deposit(juniorDeposit, 0, false, 0);
         gRouter.deposit(seniorDeposit, 0, true, 0);
         vm.stopPrank();
+
+        assertEq(dai.balanceOf(alice), aliceInitBalance - juniorDeposit - seniorDeposit);
         // Check that tranche USD balances are updated
         assertApproxEqAbs(gTranche.trancheBalances(1), seniorDeposit, 1);
         assertApproxEqAbs(gTranche.trancheBalances(0), juniorDeposit, 1);
@@ -46,15 +49,20 @@ contract RouterUnitTest is BaseUnitFixture {
         assertEq(gTranche.trancheBalances(0), 0);
         // Deal some DAI to alice
         dai.faucet(type(uint256).max / 2);
+        uint256 aliceInitBalance = dai.balanceOf(alice);
         dai.approve(address(gRouter), type(uint256).max);
         gRouter.deposit(juniorDeposit, 0, false, 0);
         gRouter.deposit(seniorDeposit, 0, true, 0);
-
+        // Check Alice balance
+        uint256 aliceBalanceSnapshot = aliceInitBalance - juniorDeposit - seniorDeposit;
+        assertEq(dai.balanceOf(alice), aliceBalanceSnapshot);
         assertApproxEqAbs(gTranche.trancheBalances(1), seniorDeposit, 1);
         // Withdraw half of senior deposit
         gRouter.withdraw(seniorDeposit / 2, 0, true, 0);
         vm.stopPrank();
 
+        // Check Alice balance in DAI
+        assertGt(dai.balanceOf(alice), aliceBalanceSnapshot);
         // Check Alice balance
         assertApproxEqAbs(gTranche.balanceOfWithFactor(alice, 1), seniorDeposit / 2, 1);
         // Check that tranche USD balances are updated
