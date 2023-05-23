a0e4da3dbe66fcc7802321e6d6bbda024afd06b1
SHAKOTN <shakotn@protonmail.com>
2023-05-23 12:06:23 +0300

    [script] - arbing script WIP

--- arb_loop.py
+++ arb_loop.py
@@ -12,25 +12,15 @@ from web3.contract import Contract
 
 # Addresses
 ADMIN = '0xBa5ED108abA290BBdFDD88A0F022E2357349566a'
-DAO = '0x359F4fe841f246a095a82cb26F5819E10a91fe0d'
-OUSD_METAPOOL = '0x87650d7bbfc3a9f10587d7778206671719d9910d'
-OUSD_FLIPPER = '0xcecaD69d7D4Ed6D52eFcFA028aF8732F27e08F70'
-CONVEX_OUSD = '0x73703f0493C08bA592AB1e321BEaD695AC5b39E3'
-OVAULT = '0xE75D77B1865Ae93c7eaa3040B038D7aA7BC02F70'
-VAULT = '0x1402c1cAa002354fC2C4a4cD2b4045A5b9625EF3'
-USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
-OUSD = '0x2a8e1e676ec238d8a992307b495b45b3feaa5e86'
-THREE_POOL_DEPOSIT_ZAP = '0xa79828df1850e8a3a3064576f380d90aecdd3359'
-THREE_POOL = '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7'
+OUSD_ARB = '0x0000000000000000000000000000000000000000'  # TODO: Change once deployed
+THREE_POOL_TOKEN = '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490'
+OUSD_STRATEGY = '0x73703f0493C08bA592AB1e321BEaD695AC5b39E3'
 
 # Constants
 CHAIN_ID = 1
 AMOUNT_TO_SWAP = int(200_000e18)
 MAX_UINT = 2 ** 256 - 100
-OUSD_META_INDEX = 0
-THREE_CURVE_INDEX = 1
+SLIPPAGE = 120
 
 
 def get_abi(contract_name: str) -> Dict:
@@ -42,20 +32,74 @@ def get_abi(contract_name: str) -> Dict:
 # Ref: https://github.com/flashbots/web3-flashbots/blob/master/examples/simple.py
 def swap() -> None:
     load_dotenv()
-    # signer: LocalAccount = Account.from_key(os.environ["ADMIN_KEY"])
-    # web3 = Web3(HTTPProvider(os.environ["WEB3_ALCHEMY_PROJECT_ID"]))
-    # # TODO: strategy.runHarvest()
-    # # !! SECTION 2: Send bundle to Flashbots
-    # # keep trying to send bundle until it gets mined
-    # while True:
-    #     block = web3.eth.block_number
-    #     print(f"Simulating on block {block}")
-    #     try:
-    #         web3.flashbots.simulate(bundle, block)
-    #         print("Simulation successful.")
-    #     except Exception as e:
-    #         print("Simulation error", e)
-    #         return
+
+    signer: LocalAccount = Account.from_key(os.environ["ADMIN_KEY"])
+    web3 = Web3(HTTPProvider(os.environ["WEB3_ALCHEMY_PROJECT_ID"]))
+    arb_ousd: Contract = web3.eth.contract(
+        address=web3.toChecksumAddress(OUSD_ARB),
+        abi=get_abi("ArbOusd")
+    )
+
+    three_pool_token: Contract = web3.eth.contract(
+        address=web3.toChecksumAddress(THREE_POOL_TOKEN),
+        abi=get_abi("ERC20")
+    )
+
+    convex_strategy: Contract = web3.eth.contract(
+        address=web3.toChecksumAddress(OUSD_STRATEGY),
+        abi=get_abi("ConvexStrategy")
+    )
+
+    flashbot(web3, signer)
+    for i in range(9):
+        bundle = []
+        nonce = web3.eth.get_transaction_count(signer.address)
+        options = {
+            "gas": 100000,
+            "maxFeePerGas": Web3.toWei(200, "gwei"),
+            "maxPriorityFeePerGas": Web3.toWei(50, "gwei"),
+            "nonce": nonce,
+            "chainId": CHAIN_ID,
+            "from": signer.address,
+        }
+
+        three_pool_allowance = three_pool_token.functions.allowance(
+            ADMIN,
+            arb_ousd.address
+        ).call()
+        if three_pool_allowance < AMOUNT_TO_SWAP:
+            approve_3crv_tx = three_pool_token.functions.approve(
+                arb_ousd.address,
+                MAX_UINT
+            ).buildTransaction(options)
+            options["nonce"] += 1
+            bundle.append({"signer": signer, "transaction": approve_3crv_tx})
+
+        # Check if arb contract has enough 3pool tokens to perform the arb
+        three_pool_balance = three_pool_token.functions.balanceOf(arb_ousd.address).call()
+        if three_pool_balance == 0:
+            arb_with_transfer_tx = arb_ousd.functions.performArbWithTransfer(
+                AMOUNT_TO_SWAP).buildTransaction(options)
+            options["nonce"] += 1
+            bundle.append({"signer": signer, "transaction": arb_with_transfer_tx})
+        else:
+            arb_tx = arb_ousd.functions.performArb(SLIPPAGE).buildTransaction(options)
+            options["nonce"] += 1
+            bundle.append({"signer": signer, "transaction": arb_tx})
+
+        # Run harvest in the end
+        harvest_tx = convex_strategy.functions.runHarvest().buildTransaction(options)
+        nonce += 1
+        bundle.append({"signer": signer, "transaction": harvest_tx})
+        while True:
+            block = web3.eth.block_number
+            print(f"Simulating on block {block}")
+            try:
+                web3.flashbots.simulate(bundle, block)
+                print("Simulation successful.")
+            except Exception as e:
+                print("Simulation error", e)
+                return
 
 
 if __name__ == "__main__":
